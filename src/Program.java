9ae88dce1fdf631d90a5118bd38c2f3c443fafdb
kiss.zoltan <kiss.zoltan@promera.hu>
2020-12-02 20:27:06 +0000

    Added improvements to look and methodized everything

--- src/Program.java
+++ src/Program.java
@@ -1,35 +1,88 @@
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.net.URL;
 
 // TODO Generate products
 // TODO Clean up - put them in separate classes
-// TODO Beautify the window and other props
 
 public class Program {
 
+    private final String version = "V1.0";
+    private final String frameTitle = "Online Shop";
+    private final String productsTabTitle = "Termékek";
+    private final String productsTabTitleDesc = "Raktárunkban megtalálható termékek";
+
+    private final String favIconPath = "resources\\images\\favicon.png";
+    private final String productsIconPath = "resources\\images\\productsIcon.png";
+
+    private JFrame window;
+    private JTabbedPane products;
+    private JPanel productsHolder;
+    private JScrollPane listOfProducts;
+
     public void run(){
 
-        JFrame window = new JFrame("Online Shop");
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        createFrame();
+
+        createProductsTab();
+
+        window.setVisible(true);
+    }
+
+    /**
+     * Creates the initial frame of the program
+     * Initializes: close operation, sizes, icon, position
+     */
+    private void createFrame(){
+        window = new JFrame(frameTitle + " " + version);        // creates Frame
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // What should the X do?
+
+        window.setMinimumSize(new Dimension(800, 400));
+        window.setMaximumSize(new Dimension(1500, 1000));
         window.setSize(1000,500);
 
-        JTabbedPane products = new JTabbedPane();
+        ImageIcon icon = new ImageIcon(favIconPath);
+        window.setIconImage(icon.getImage());                   // Sets icon
+
+        window.setLocationRelativeTo(null);                     // Set frame to the center of screen
+
+    }
+
+    /**
+     * Creates the products tab, which displays
+     * the available products in the shop
+     */
+    private void createProductsTab(){
+        products = new JTabbedPane();
         products.setTabLayoutPolicy(JTabbedPane.WRAP_TAB_LAYOUT);
         products.setTabPlacement(JTabbedPane.TOP);
 
-        JPanel p = new JPanel();
+        ImageIcon icon = new ImageIcon(productsIconPath); // Sets icon
+
+        createProductsHolder();
+
+        createProductsScrollPane();
+
+        products.addTab(productsTabTitle, icon, listOfProducts, productsTabTitleDesc);
+
+        window.add(products);
+    }
+
+    /**
+     * Creates the panel, which makes the products
+     * display as a list
+     */
+    private void createProductsHolder(){
+        productsHolder = new JPanel();
+        productsHolder.setLayout(new GridBagLayout());
+
         GridBagConstraints c = new GridBagConstraints();
-        c.fill = GridBagConstraints.VERTICAL;
 
-        p.setLayout(new GridBagLayout());
-
-
-        for(int i = 0; i < 50; ++i){
+        for(int i = 0; i < 60; ++i){
             JPanel temp = new JPanel(new GridBagLayout());
-            temp.setSize(500, 500);
 
-            if(i %2 == 0){
+            if(i%2 == 0){
                 temp.setBackground(Color.BLACK);
                 temp.add(new JLabel("TEST"));
             }
@@ -38,22 +91,20 @@ public class Program {
                 temp.add(new JLabel("TEST"));
             }
             c.ipady = 50;
-            c.anchor = GridBagConstraints.CENTER;
-            c.weighty = 0.7;
-            c.weightx = 0.1;
-            c.gridx = 0;
-            c.gridy = i;
+            c.anchor = GridBagConstraints.NORTH;
+            c.weightx = 1;
+            c.gridx = 1;
+            c.insets = new Insets(5,5,5,5);
             c.fill = GridBagConstraints.HORIZONTAL;
-            p.add(temp,c);
-         }
+            productsHolder.add(temp,c);
+        }
+    }
 
-        JScrollPane listOfProducts = new JScrollPane(p);
+    /**
+     * Creates a scrollable environment for the list
+     */
+    private void createProductsScrollPane(){
+        listOfProducts = new JScrollPane(productsHolder);
         listOfProducts.getVerticalScrollBar().setUnitIncrement(16);
-
-        products.addTab("Products", listOfProducts);
-        // TODO Rewrite the tab title to Hungarian
-
-        window.add(products);
-        window.setVisible(true);
     }
 }
